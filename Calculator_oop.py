b1cdd69eb32fb608b4d80330a70986fe1723f4b4
Priya bhardwaj <priyabhardwaj0214@gmail.com>
2025-11-30 13:39:25 +0530

    Add files via upload

---
+++ Calculator_oop.py
@@ -0,0 +1,28 @@
+class Calculator:
+    def _init_(self):
+        print("Calculator Started!")
+
+    def add(self, a, b):
+        return a + b
+    
+    def subtract(self, a, b):
+        return a - b
+    
+    def multiply(self, a, b):
+        return a * b
+    
+    def divide(self, a, b):
+        if b == 0:
+            return "Cannot divide by zero!"
+        return a / b
+
+
+# ---- Main Program ----
+calc = Calculator()
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+print("Addition:", calc.add(a, b))
+print("Subtraction:", calc.subtract(a, b))
+print("Multiplication:", calc.multiply(a, b))
+print("Division:", calc.divide(a, b))
